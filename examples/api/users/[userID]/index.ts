1a7f09503f67729b476e85b304210c171ed53e04
jheising <jheising@gmail.com>
2023-08-31 19:04:35 -0700

    Updates

--- examples/api/users/[userID]/index.ts
+++ examples/api/users/[userID]/index.ts
@@ -1,12 +1,12 @@
-import { BearerToken, GET, AllHeaders, Logger, PlatAPILogger, Request, Header } from "../../../../src";
+import { BearerToken, GET, AllHeaders, Logger, PlatAPILogger, Request } from "../../../../src";
 import { User } from "../../../src/User";
 
 export default class SampleAPI {
     @GET
     static async getUserByID(
         userID: string,
-        @Header
-        blah: any
+        @AllHeaders
+        headers: any
     ): Promise<User> {
         return {
             id: userID,

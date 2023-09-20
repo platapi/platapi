8a0271c56dc5c401ba8bd1c0cb69d2b3b6fdbc7b
jheising <jheising@gmail.com>
2023-09-20 08:12:46 -0700

    Fixes to catch-all routes

--- examples/api/events/[[...eventType]]/index.ts
+++ examples/api/events/[[...eventType]]/index.ts
@@ -1,13 +1,14 @@
-import { Body, Path, POST } from "../../../../src";
+import { Body, Optional, Path, POST } from "../../../../src";
 
 export default class SampleAPI {
     @POST
     static async saveEvent(
         @Path
+        @Optional
         eventType: string,
         @Body
         event: any
     ): Promise<string> {
-        return eventType;
+        return eventType ?? "none";
     }
 }
